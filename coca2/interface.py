751c403b070517c053e02bc7e04f5bca5f7a5255
Sparger, John E <spargerje@ornl.gov>
2016-12-05 16:27:30 -0500

    working on a less confusing version of coca which will also accept writes from epics.

---
+++ coca2/interface.py
@@ -0,0 +1,46 @@
+from multiprocessing.managers import BaseManager 
+
+# need some locks here
+class CocaMPInterface(object):
+	pvs = {}
+	new_pvs = []
+
+	def get_pv_dict(self):
+		return self.pvs
+
+	def get_new(self):
+		return self.new_pvs
+
+	def clear_new(self):
+		if self.new_pvs:
+			self.new_pvs = []
+
+	def broadcast_pv(self,pv):
+		self.new_pvs.append(pv.name)
+		self.update_pv(pv)
+
+	def update_pv(self,pv):
+		self.pvs[pv.name] = pv
+
+	def get_pv(self,name):
+		return self.pvs[name]
+
+	def get_pv_value(self,name):
+		return self.pvs[name].value
+
+	def set_pv_value(self,name,value):
+		self.pvs[name].value = value
+
+
+class CocaMPManager(BaseManager):
+	pass
+
+CocaMPManager.register('CocaMPInterface',CocaMPInterface)
+
+manager = CocaMPManager()
+manager.start()
+interface = manager.CocaMPInterface()
+
+def broadcast_pv(pv):
+	global interface
+	interface.broadcast_pv(pv)
---
+++ coca2/run.py
@@ -0,0 +1,19 @@
+from multiprocessing import Process
+from interface import interface
+from pv import PV
+
+def run(interface):
+	# start the coca server
+	import driverMP as driver
+	driver.interface = interface
+	# create a status pv
+	# pvA = driver.Data("coca:running", value=1, meta={'scan': 1})
+	# driver.broadcast_python_pv(pvA)
+	# let server continue to run
+	driver.t.join()
+
+# start the coca process
+p = Process(target=run, args=(interface,)); p.daemon=True; p.start()
+
+pv = PV("coca:running", value=1, meta={'scan': 1})
+interface.broadcast_pv(pv)
