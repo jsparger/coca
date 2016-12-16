d5314dcf2493358c107773494656b4d8dcaaa0c8
Sparger, John E <spargerje@ornl.gov>
2016-12-16 11:58:04 -0500

    added logging to driver process for debugging

--- coca/run.py
+++ coca/run.py
@@ -2,8 +2,22 @@ from multiprocessing import Process
 from interface import interface
 from pv import PV
 import time
+import os
+import sys
+
+def info(title):
+    print title
+    print 'module name:', __name__
+    print 'parent process:', os.getppid()
+    print 'process id:', os.getpid()
 
 def run(interface):
+	# set up logging
+	procname = 'cocadriver'
+	sys.stdout = open(procname + ".out", "w", buffering=0)
+	sys.stderr = open(procname + ".err", "w", buffering=0)
+	info(procname)
+
 	# start the coca server
 	import driver
 	driver.interface = interface
