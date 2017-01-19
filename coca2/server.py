fed5492d83dc309ee72c7e0311ccc5080c043a64
Sparger, John E <spargerje@ornl.gov>
2017-01-18 21:43:08 -0500

    fixed check for existing manager server

--- coca2/server.py
+++ coca2/server.py
@@ -1,9 +1,10 @@
-from .remote_pv import RemotePV
-from .proxy import StableEventProxy, StableLockProxy
+from remote_pv import RemotePV
+from proxy import StableEventProxy, StableLockProxy
 import multiprocessing
 import threading
 import Queue
 from multiprocessing.managers import SyncManager
+import socket
 
 # A synchronized queue to hold new pvs.
 new_pv_queue = Queue.Queue()
@@ -19,7 +20,7 @@ class CocaInterface(object):
 		censored.pop("lock",None)
 		return censored
 
-	def broadcast_pv(self,pv):
+	def broadcast_pv(self, pv):
 		if pv.name in self.pvs:
 			return False
 		with self.lock:
@@ -32,28 +33,32 @@ class CocaInterface(object):
 			return False
 		return self.pvs[name]
 
-	def read(self.name):
+	def read(self, name):
 		# danger
 		return self.pvs[name].read()
 
-	def write(self.name,value):
+	def write(self, name, value):
 		# danger
 		self.pvs[name].write()
 
 interface = CocaInterface()
 
+coca_address = ('localhost',5050)
+
 def get_manager(klass):
 	manager = klass(('localhost',5050))
-	print "manager = {}".format(manager)
 	try:
-		print "trying to start"
-		manager = klass(('localhost',5050))
-		manager.start()
-	except:
-		print "already started. Trying to connect"
-		manager = klass(('localhost',5050))
+		# check to see if there is a server running already
+		s = socket.socket()
+		s.bind(coca_address)
+		s.close()
+	except socket.error as e:
+		# server already running, so just connect
 		manager.connect()
-		print "done"
+	else:
+		# server not running, so start
+		manager.start()
+
 	return manager
 
 class Manager(SyncManager):
@@ -66,3 +71,4 @@ Manager.register("get_interface", lambda: interface)
 Manager.register("get_new_pv_queue", lambda: new_pv_queue)
 
 manager = get_manager(Manager)
+
--- coca2/untitled.py
+++ coca2/untitled.py
@@ -2,6 +2,8 @@
 
 import multiprocessing
 from multiprocessing.managers import SyncManager
+import threading
+from proxy import StableLockProxy
 
 class Dog(object):
 	def __init__(self):
@@ -11,6 +13,16 @@ class Dog(object):
 		self.name = name
 
 	def get_name(self):
+		try:
+			m = SyncManager(('localhost',5050),authkey='abracadabra')
+			m.register("StableLock")
+			m.connect()
+			print "worked fine"
+			l = m.StableLock()
+			print l
+		except Exception as e:
+			print e
+			print "wtf"
 		return self.name
 
 dog = Dog()
@@ -21,9 +33,13 @@ class Manager(SyncManager):
 
 Manager.register("Dog",lambda: dog)
 Manager.register("Sync",lambda: sync)
+Manager.register("StableLock", threading.Lock, StableLockProxy)
+
 
 m = Manager(('localhost',5050),authkey='abracadabra')
 m.start()
+l = m.StableLock()
+print l
 dogproxy = m.Dog()
 print dogproxy.get_name()
 dogproxy.set_name("Spike")
